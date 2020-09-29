a593092d8cd51524f11f3b2c3781f0ce37274b21
Vasilii Trofimchuk <vthub.inbox@gmail.com>
2020-09-29 16:52:20 -0700

    Fix image

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Container from "react-bootstrap/cjs/Container";
+import logo from './logo512.png'
 import Debug from "./Debug";
 
 function App() {
   return (
     <Container>
       <div className="py-5 text-center">
-        <img className="d-block mx-auto mb-4" src="/logo512.png" alt="" width="72" height="72"/>
+        <img className="d-block mx-auto mb-4" src={logo} alt="" width="72" height="72"/>
           <h1>Webauthn Payload Debugger</h1>
           <p className="lead">Webauthn (a.k.a. FIDO2) is a protocol to enable authentication using private-public key pairs.</p>
       </div>
